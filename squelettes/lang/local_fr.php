d1448ca2309fb9506fe9095177906cd41dba0e45
christophe ld <christophe@rouge-rose.net>
2010-04-19 13:29:35 +0200

    ajouts recherche et raccourcis accessibilité

---
+++ squelettes/lang/local_fr.php
@@ -0,0 +1,55 @@
+<?php
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+    // A
+    'afficher_texte_complet' => 'Afficher le texte complet',
+    '_article' => 'article',
+    '_articles' => 'articles',
+    'article_in_rubrique' => 'Article publi&eacute; dans la rubrique',
+    
+    // D
+    'dernieres_actualites' => 'Derni&egrave;res actualit&eacute;s',
+    
+    // E
+    'en_savoir_plus' => 'En savoir plus',
+    
+	// H
+	'help' => 'Aide',
+	'haut_page' => "Haut de page",
+	
+	// I
+	'item' => '&eacute;lement',
+	'items' => '&eacute;lements',
+	
+	// M
+	'map' => 'Plan du site',
+	
+	// N
+	'navigation' => 'Navigation',
+	
+	// P
+	'page' => 'Page',
+	'par_motcle' => 'Par mot-cl&eacute;',
+	'publie_le' => 'Publi&eacute; le',
+	
+	//R
+	'_rubrique' => 'rubrique',
+	'_rubriques' => 'rubriques',
+	
+	// S
+	'signatures' => 'signatures',
+	'signer_petition' => 'Signer la p&eacute;tition',
+	'_site_web' => 'site web',
+	'_sites_web' => 'sites web',
+	'skip_contenu' => 'Aller au contenu',
+	'skip_navigation' => 'Aller &agrave; la navigation',
+	'skip_search' => 'Aller &agrave; la recherche',
+	'sur' => 'sur',
+	
+	// T
+	'tous_articles_in_rubrique' => 'Tous les articles publi&eacute;s dans la rubrique',
+	
+	// V
+	'voir_signatures' => 'Voir les signatures'
+	
+	);
+?>
